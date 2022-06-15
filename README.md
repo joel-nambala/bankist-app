d0f491e53086646807cca52a84ebbe1b2e1a2e33
Joel Wanjala <joel.n.wanjala@gmail.com>
2022-06-15 10:06:06 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# bankist-app
+# Bankist Application
+
+This bankist application I made together with Jonas on udemy. It simulates the real banking environment where there are deposits, withdrawals, loans and transfering money from one user to another.
---
+++ script.js
@@ -0,0 +1,83 @@
+'use strict';
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// DOM Elements
+const labelBalance = document.querySelector('.balance__total');
+
+const containeMovements = document.querySelector('.movements');
+
+const displayMovements = function (movements) {
+  // 1. Clear the movements container
+  containeMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    // 2. Check the type of the movement
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    // 3. Create a HTML string
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}</div>
+      </div>
+    `;
+
+    // 4. Append the movements to the UI
+    containeMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+
+  labelBalance.textContent = `${balance} USD`;
+};
+
+calcDisplayBalance(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+console.log(accounts);
